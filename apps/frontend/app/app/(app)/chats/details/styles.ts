836ee301b18593628b2cf05977e8b17ddfea796b
NilsBaumgartner1994 <33295329+NilsBaumgartner1994@users.noreply.github.com>
2025-07-16 11:19:46 +0200

    Add experimental chat screens

---
+++ apps/frontend/app/app/(app)/chats/details/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  list: {
+    padding: 20,
+    gap: 10,
+  },
+  messageItem: {
+    padding: 10,
+    borderRadius: 10,
+  },
+  timestamp: {
+    fontSize: 12,
+    fontFamily: 'Poppins_400Regular',
+    marginBottom: 4,
+  },
+});
